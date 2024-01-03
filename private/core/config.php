688682cc94b236221d0415b80156364a1be28614
SETYA HADI NUGROHO <222112358@stis.ac.id>
2024-01-03 10:36:23 +0700

    first commit

---
+++ private/core/config.php
@@ -0,0 +1,10 @@
+<?php
+
+define('ROOT', 'http://localhost:85/apk-ujian/public');
+define('ASSETS', 'http://localhost:85/apk-ujian/public/assets');
+
+define('DBNAME', 'school_db');
+define('DBHOST', 'localhost');
+define('DBUSER', 'root');
+define('DBPASS', '');
+define('DBDRIVER', 'mysql');
